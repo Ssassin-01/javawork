d4888222fbbc26b3e292ad401f4fe8cbbd8962c5
Ssassin-01 <71809583+Ssassin-01@users.noreply.github.com>
2024-12-23 16:05:32 +0900

    12.23.3교시

---
+++ java/src/chap01_print/Print01.java
@@ -0,0 +1,16 @@
+package chap01_print;
+
+public class Print01 {
+    public static void main(String[] args) {
+        // 한줄 주석 : 컴파일 되지 않는다(실행이 되지 않는다)
+        // 한줄 주석의 단축키 ctrl + /
+        /* 여러줄 주석
+           여기부터
+           여기까지 주석
+           단축키 : 블러긍 ㄹ잡고 ctrl + shift + /*/
+        //화면에 출력해주는 프로글매
+        System.out.println("본인이름 : 양제훈");
+        System.out.println("더조은 아카데미 학원");
+        System.out.println("강남에서 제일 좋은 학원");
+    }
+}
